0645553450abed9f204cfc64db77b24cf6616461
Chinmay Sai <yanamalachinmaysai106@gmail.com>
2025-01-15 19:24:03 +0530

    EX_3_7_AutoWired_Annotation

---
+++ 3_7_AutoWired_Annotation/src/main/java/com/chinmay/componentannotation/Alien.java
@@ -0,0 +1,53 @@
+
+package com.chinmay.componentannotation;
+
+import java.beans.ConstructorProperties;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Alien {
+	
+	private int age;
+	@Autowired         //Field Injection
+	@Qualifier("com1")
+	//@Qualifier("desktop")
+	private Computer com;
+
+
+	public Computer getCom() {
+		return com;
+	}
+
+	//@Autowired             //Setter Injection
+	//@Qualifier("com1")
+	public void setCom(Computer com) {
+		this.com = com;
+	}
+
+		public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		//System.out.println("Inside the Setter Method");
+		this.age = age;
+	}
+
+	public Alien() {
+	System.out.println("Alien Object Created");
+	}
+	//@Autowired   //Constructor Injection
+	public Alien(Computer com)
+	{
+		this.com=com;
+	}
+
+	public void code()
+	{
+	System.out.println("Coding");
+	com.compile();
+	}
+}
---
+++ 3_7_AutoWired_Annotation/src/main/java/com/chinmay/componentannotation/Desktop.java
@@ -0,0 +1,17 @@
+package com.chinmay.componentannotation;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Desktop implements Computer {
+
+	public Desktop()
+	{
+		System.out.println("Desktop Object/bean Created");
+	}
+	@Override
+	public void compile()
+	{
+		System.out.println("Desktop Code Compilation is in Progress");
+	}
+}
