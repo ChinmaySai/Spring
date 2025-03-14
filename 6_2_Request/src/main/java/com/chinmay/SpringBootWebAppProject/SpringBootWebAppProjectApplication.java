fbe2612721fae9165f966162cac01afa03f0e574
Chinmay Sai <yanamalachinmaysai106@gmail.com>
2025-03-14 20:49:31 +0530

    Committing Request Param Annotation Changes

---
+++ 6_2_Request/src/main/java/com/chinmay/SpringBootWebAppProject/SpringBootWebAppProjectApplication.java
@@ -0,0 +1,13 @@
+package com.chinmay.SpringBootWebAppProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebAppProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebAppProjectApplication.class, args);
+	}
+
+}
