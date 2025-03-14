8fbf4aca665b765abee9111cd8c7c6c8e3e0143b
Chinmay Sai <yanamalachinmaysai106@gmail.com>
2025-03-14 20:55:10 +0530

    As the variable name does not match we are using @RequestParam
    annotation to map the values

--- 6_2_Request/src/main/java/com/chinmay/SpringBootWebAppProject/HomeController.java
+++ 6_2_Request/src/main/java/com/chinmay/SpringBootWebAppProject/HomeController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
@@ -25,13 +26,22 @@ public class HomeController {
         hs.setAttribute("result",(num1+num2));
 		return "result.jsp";
 	}*/
+//	@RequestMapping("add")
+//	public String add(int num1,int num2,HttpSession hs)
+//	{
+//		//int num1=Integer.parseInt(req.getParameter("num1"));
+//		//int num2=Integer.parseInt(req.getParameter("num2"));
+//        System.out.println("Addition Result "+(num1+num2));
+//        hs.setAttribute("result",(num1+num2));
+//		return "result.jsp";
+//	}
 	@RequestMapping("add")
-	public String add(int num1,int num2,HttpSession hs)
+	public String add(@RequestParam("num1") int num,int num2,HttpSession hs)
 	{
 		//int num1=Integer.parseInt(req.getParameter("num1"));
 		//int num2=Integer.parseInt(req.getParameter("num2"));
-        System.out.println("Addition Result "+(num1+num2));
-        hs.setAttribute("result",(num1+num2));
+        System.out.println("Addition Result "+(num+num2));
+        hs.setAttribute("result",(num+num2));
 		return "result.jsp";
 	}
 
