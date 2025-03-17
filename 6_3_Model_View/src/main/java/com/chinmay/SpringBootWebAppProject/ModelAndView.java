ef301cd518e4515c7666fde621f9b45e1a22b6df
Chinmay Sai <yanamalachinmaysai106@gmail.com>
2025-03-17 15:23:46 +0530

    ModelAndView Code Changes Commit

---
+++ 6_3_Model_View/src/main/java/com/chinmay/SpringBootWebAppProject/ModelAndView.java
@@ -0,0 +1,13 @@
+package com.chinmay.SpringBootWebAppProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelAndView {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelAndView.class, args);
+	}
+
+}
