ae94a95e2c326cd9a798f334f7216b615a7573ee
Chinmay Sai <yanamalachinmaysai106@gmail.com>
2025-03-17 14:13:01 +0530

    Adding the data to the Model Object and JSP will retrieve the data.

--- 6_2_Request/src/main/java/com/chinmay/SpringBootWebAppProject/HomeController.java
+++ 6_2_Request/src/main/java/com/chinmay/SpringBootWebAppProject/HomeController.java
@@ -1,6 +1,7 @@
 package com.chinmay.SpringBootWebAppProject;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,13 +36,25 @@ public class HomeController {
 //        hs.setAttribute("result",(num1+num2));
 //		return "result.jsp";
 //	}
+//	@RequestMapping("add")
+//	public String add(@RequestParam("num1") int num,int num2,HttpSession hs)
+//	{
+//		//int num1=Integer.parseInt(req.getParameter("num1"));
+//		//int num2=Integer.parseInt(req.getParameter("num2"));
+//        System.out.println("Addition Result "+(num+num2));
+//        hs.setAttribute("result",(num+num2));
+//		return "result.jsp";
+//	}
+	
+	//Transferring data to the client using the Model Object
 	@RequestMapping("add")
-	public String add(@RequestParam("num1") int num,int num2,HttpSession hs)
+	public String add(@RequestParam("num1") int num,int num2,Model model)
 	{
 		//int num1=Integer.parseInt(req.getParameter("num1"));
 		//int num2=Integer.parseInt(req.getParameter("num2"));
         System.out.println("Addition Result "+(num+num2));
-        hs.setAttribute("result",(num+num2));
+       model.addAttribute("result",(num+num2));
+    //    hs.setAttribute("result",(num+num2));
 		return "result.jsp";
 	}
 
